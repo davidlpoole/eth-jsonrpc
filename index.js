1fef026edd9b5e84adff17c299a45236a272bbdb
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-07-15 13:29:24 +0100

    Test sending a Signed JSON-RPC Request (a transaction) on Sepolia using the Alchemy SDK

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 require('dotenv').config();
-const axios = require('axios');
+const { Alchemy, Network, Wallet, Utils } = require('alchemy-sdk');
 
-const ALCHEMY_URL = process.env.ALCHEMY_URL;
-const SEPOLIA_WALLET = process.env.SEPOLIA_WALLET;
+const settings = {
+    apiKey: process.env.TEST_API_KEY,
+    network: Network.ETH_SEPOLIA,
+};
 
-axios.post(ALCHEMY_URL, {
-    jsonrpc: "2.0",
-    id: 1,
-    method: "eth_getBalance",
-    params: [
-        SEPOLIA_WALLET, // address
-        "latest" // latest block
-    ]
-}).then((response) => {
-    console.log(response.data.result);
-});
+const alchemy = new Alchemy(settings);
+
+let wallet = new Wallet(process.env.TEST_PRIVATE_KEY);
+
+async function main() {
+    const nonce = await alchemy.core.getTransactionCount(
+        wallet.address,
+        'latest'
+    );
+
+    let transaction = {
+        to: process.env.SEPOLIA_WALLET,
+        value: Utils.parseEther('0.001'), // 0.001 worth of ETH being sent
+        gasLimit: '21000',
+        maxPriorityFeePerGas: Utils.parseUnits('5', 'gwei'),
+        maxFeePerGas: Utils.parseUnits('20', 'gwei'),
+        nonce: nonce,       // transaction count
+        type: 2,            // not a legacy tx
+        chainId: 11155111,  // Ethereum sepolia testnet transaction
+}
+
+    let rawTransaction = await wallet.signTransaction(transaction);
+    console.log('Raw tx: ', rawTransaction);
+    let tx = await alchemy.core.sendTransaction(rawTransaction);
+    console.log(`https://sepolia.etherscan.io/tx/${tx.hash}`);
+}
+
+main();
